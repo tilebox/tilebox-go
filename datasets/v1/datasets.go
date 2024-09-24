ae3aee99e092ed9fe27bd02adfdcaaa7f61f99dc
Corentin <33163342+corentinmusard@users.noreply.github.com>
2024-09-24 12:31:17 +0000

    Add collection service methods (#39)

--- datasets/v1/datasets.go
+++ datasets/v1/datasets.go
@@ -57,6 +57,66 @@ func NewDatasetsService(client datasetsv1connect.TileboxServiceClient, options .
 	}
 }
 
+func (s *Service) CreateCollection(ctx context.Context, datasetID uuid.UUID, collectionName string) (*datasetsv1.CollectionInfo, error) {
+	return observability.WithSpanResult(ctx, s.tracer, "datasets/create_collection", func(ctx context.Context) (*datasetsv1.CollectionInfo, error) {
+		res, err := s.client.CreateCollection(ctx, connect.NewRequest(
+			&datasetsv1.CreateCollectionRequest{
+				DatasetId: &datasetsv1.ID{
+					Uuid: datasetID[:],
+				},
+				Name: collectionName,
+			},
+		))
+
+		if err != nil {
+			return nil, fmt.Errorf("failed to create collection: %w", err)
+		}
+
+		return res.Msg, nil
+	})
+}
+
+func (s *Service) GetCollections(ctx context.Context, datasetID uuid.UUID) (*datasetsv1.Collections, error) {
+	return observability.WithSpanResult(ctx, s.tracer, "datasets/get_collections", func(ctx context.Context) (*datasetsv1.Collections, error) {
+		res, err := s.client.GetCollections(ctx, connect.NewRequest(
+			&datasetsv1.GetCollectionsRequest{
+				DatasetId: &datasetsv1.ID{
+					Uuid: datasetID[:],
+				},
+				WithAvailability: true,
+				WithCount:        true,
+			},
+		))
+
+		if err != nil {
+			return nil, fmt.Errorf("failed to get collections: %w", err)
+		}
+
+		return res.Msg, nil
+	})
+}
+
+func (s *Service) GetCollectionByName(ctx context.Context, datasetID uuid.UUID, collectionName string) (*datasetsv1.CollectionInfo, error) {
+	return observability.WithSpanResult(ctx, s.tracer, "datasets/get_collection_by_name", func(ctx context.Context) (*datasetsv1.CollectionInfo, error) {
+		res, err := s.client.GetCollectionByName(ctx, connect.NewRequest(
+			&datasetsv1.GetCollectionByNameRequest{
+				CollectionName:   collectionName,
+				WithAvailability: true,
+				WithCount:        true,
+				DatasetId: &datasetsv1.ID{
+					Uuid: datasetID[:],
+				},
+			},
+		))
+
+		if err != nil {
+			return nil, fmt.Errorf("failed to get collections: %w", err)
+		}
+
+		return res.Msg, nil
+	})
+}
+
 func (s *Service) Ingest(ctx context.Context, collectionID uuid.UUID, datapoints *datasetsv1.Datapoints, allowExisting bool) (*datasetsv1.IngestDatapointsResponse, error) {
 	return observability.WithSpanResult(ctx, s.tracer, "datasets/save", func(ctx context.Context) (*datasetsv1.IngestDatapointsResponse, error) {
 		res, err := s.client.IngestDatapoints(ctx, connect.NewRequest(
