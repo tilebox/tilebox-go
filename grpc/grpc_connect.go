18d791bb67afebd012fdcd859796a84075bd7ed8
Corentin Musard <corentin.musard@tilebox.com>
2025-03-11 17:14:56 +0100

    Don't log requests by default

--- grpc/grpc_connect.go
+++ grpc/grpc_connect.go
@@ -46,6 +46,7 @@ func RetryOnStatusUnavailable(ctx context.Context, resp *http.Response, err erro
 
 func RetryHTTPClient() connect.HTTPClient {
 	retryClient := retryablehttp.NewClient()
+	retryClient.Logger = nil
 	retryClient.RetryWaitMin = 20 * time.Millisecond
 	retryClient.RetryWaitMax = 10 * time.Second
 	retryClient.RetryMax = 5
