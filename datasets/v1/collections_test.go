6022930a6c70a00dca619a89f07a97f9449a9395
Corentin Musard <corentin.musard@tilebox.com>
2025-03-14 16:49:26 +0100

    Split tests

---
+++ datasets/v1/collections_test.go
@@ -0,0 +1,48 @@
+package datasets
+
+import (
+	"context"
+	"testing"
+
+	"github.com/samber/lo"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_collectionClient_Get(t *testing.T) {
+	ctx := context.Background()
+	client, err := NewReplayClient(t, "collection")
+	if err != nil {
+		t.Fatalf("Failed to create client: %v", err)
+	}
+
+	dataset, err := client.Datasets.Get(ctx, "open_data.asf.ers_sar")
+	require.NoError(t, err)
+
+	collection, err := client.Collections.Get(ctx, dataset.ID, "ERS-2")
+	require.NoError(t, err)
+
+	assert.Equal(t, "ERS-2", collection.Name)
+	assert.Equal(t, "c408f2b8-0488-4528-9fb7-a18361df639f", collection.ID.String())
+	assert.Equal(t, "1995-10-01 03:13:03 +0000 UTC", collection.Availability.Start.String())
+	assert.NotZero(t, collection.Count)
+}
+
+func Test_collectionClient_List(t *testing.T) {
+	ctx := context.Background()
+	client, err := NewReplayClient(t, "collections")
+	if err != nil {
+		t.Fatalf("Failed to create client: %v", err)
+	}
+
+	dataset, err := client.Datasets.Get(ctx, "open_data.asf.ers_sar")
+	require.NoError(t, err)
+
+	collections, err := client.Collections.List(ctx, dataset.ID)
+	require.NoError(t, err)
+
+	names := lo.Map(collections, func(c *Collection, _ int) string {
+		return c.Name
+	})
+	assert.Equal(t, []string{"ERS-1", "ERS-2"}, names)
+}
--- datasets/v1/datapoints_test.go
+++ datasets/v1/datapoints_test.go
@@ -2,6 +2,7 @@ package datasets
 
 import (
 	"context"
+	"iter"
 	"testing"
 	"time"
 
@@ -174,4 +175,168 @@ func Benchmark_LoadInto(b *testing.B) {
 		}
 	})
 	resultLoadInto = datapoints
+}
+
+func Test_datapointClient_Load(t *testing.T) {
+	ctx := context.Background()
+	client, err := NewReplayClient(t, "load")
+	if err != nil {
+		t.Fatalf("Failed to create client: %v", err)
+	}
+
+	dataset, err := client.Datasets.Get(ctx, "open_data.asf.ers_sar")
+	require.NoError(t, err)
+
+	collection, err := client.Collections.Get(ctx, dataset.ID, "ERS-2")
+	require.NoError(t, err)
+	assert.Equal(t, "ERS-2", collection.Name)
+
+	jan2000 := time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC)
+	interval := NewStandardTimeInterval(jan2000, jan2000.AddDate(0, 0, 7))
+
+	t.Run("CollectAs", func(t *testing.T) {
+		datapoints, err := CollectAs[*datasetsv1.ASFSarGranule](client.Datapoints.Load(ctx, collection.ID, interval))
+		require.NoError(t, err)
+
+		assert.Len(t, datapoints, 298)
+		assert.Equal(t, "00dc7952-6c90-f40d-9b5e-3b72d4e790e0", datapoints[0].Meta.ID.String())
+		assert.Equal(t, "2000-01-03 19:37:30 +0000 UTC", datapoints[0].Meta.EventTime.String())
+		assert.Equal(t, "E2_24602_STD_F619", datapoints[0].Data.GetGranuleName())
+	})
+
+	t.Run("CollectAs WithSkipData", func(t *testing.T) {
+		datapoints, err := CollectAs[*datasetsv1.ASFSarGranule](client.Datapoints.Load(ctx, collection.ID, interval, WithSkipData()))
+		require.NoError(t, err)
+
+		assert.Len(t, datapoints, 298)
+		assert.Equal(t, "00dc7952-6c90-f40d-9b5e-3b72d4e790e0", datapoints[0].Meta.ID.String())
+		assert.Empty(t, datapoints[0].Data.GetGranuleName())
+	})
+
+	t.Run("CollectAs WithSkipMeta", func(t *testing.T) {
+		datapoints, err := CollectAs[*datasetsv1.ASFSarGranule](client.Datapoints.Load(ctx, collection.ID, interval, WithSkipMeta()))
+		require.NoError(t, err)
+
+		assert.Len(t, datapoints, 298)
+		assert.Equal(t, "00dc7952-6c90-f40d-9b5e-3b72d4e790e0", datapoints[0].Meta.ID.String())
+		assert.Empty(t, datapoints[0].Meta.EventTime)
+		assert.Equal(t, "E2_24602_STD_F619", datapoints[0].Data.GetGranuleName())
+	})
+
+	t.Run("CollectAs WithSkipData WithSkipMeta", func(t *testing.T) {
+		datapoints, err := CollectAs[*datasetsv1.ASFSarGranule](client.Datapoints.Load(ctx, collection.ID, interval, WithSkipData(), WithSkipMeta()))
+		require.NoError(t, err)
+
+		assert.Len(t, datapoints, 298)
+		assert.Equal(t, "00dc7952-6c90-f40d-9b5e-3b72d4e790e0", datapoints[0].Meta.ID.String())
+		assert.Empty(t, datapoints[0].Meta.EventTime)
+		assert.Empty(t, datapoints[0].Data.GetGranuleName())
+	})
+}
+
+type mockService struct {
+	meta []*DatapointMetadata
+	data [][]byte
+
+	DatapointsClient
+}
+
+func NewMockDatapointsClient(tb testing.TB, n int) DatapointsClient {
+	// generate some mock data
+	meta := make([]*DatapointMetadata, n)
+	data := make([][]byte, n)
+	for i := range n {
+		id := uuid.New()
+		meta[i] = &DatapointMetadata{
+			ID: id,
+		}
+
+		datapoint := &datasetsv1.CopernicusDataspaceGranule{
+			GranuleName:     id.String(),
+			ProcessingLevel: datasetsv1.ProcessingLevel_PROCESSING_LEVEL_L1,
+			Satellite:       "Sentinel-1",
+			FlightDirection: datasetsv1.FlightDirection_FLIGHT_DIRECTION_ASCENDING,
+		}
+
+		message, err := proto.Marshal(datapoint)
+		if err != nil {
+			tb.Fatalf("failed to marshal datapoint: %v", err)
+		}
+
+		data[i] = message
+	}
+
+	return &mockService{
+		meta: meta,
+		data: data,
+	}
+}
+
+func (s *mockService) Load(_ context.Context, _ uuid.UUID, _ LoadInterval, _ ...LoadOption) iter.Seq2[*RawDatapoint, error] {
+	return func(yield func(*RawDatapoint, error) bool) {
+		for i := range s.meta {
+			datapoint := &RawDatapoint{
+				Meta: s.meta[i],
+				Data: s.data[i],
+			}
+			if !yield(datapoint, nil) {
+				return
+			}
+		}
+	}
+}
+
+// result is used to avoid the compiler optimizing away the benchmark output
+var result []*TypedDatapoint[*datasetsv1.CopernicusDataspaceGranule]
+
+// BenchmarkCollectAs benchmarks the CollectAs function
+// It is used to benchmark the cost of reflection and proto.Marshal inside CollectAs
+func BenchmarkCollectAs(b *testing.B) {
+	ctx := context.Background()
+	collectionID := uuid.New()             // dummy collection ID
+	loadInterval := newEmptyTimeInterval() // dummy load interval
+
+	client := NewClient()
+	client.Datapoints = NewMockDatapointsClient(b, 1000)
+
+	var r []*TypedDatapoint[*datasetsv1.CopernicusDataspaceGranule] // used to avoid the compiler optimizing the output
+	b.Run("CollectAs", func(b *testing.B) {
+		for range b.N {
+			data := client.Datapoints.Load(ctx, collectionID, loadInterval)
+			r, _ = CollectAs[*datasetsv1.CopernicusDataspaceGranule](data)
+		}
+	})
+	result = r
+
+	b.Run("Marshal and no reflection", func(b *testing.B) {
+		for range b.N {
+			data := client.Datapoints.Load(ctx, collectionID, loadInterval)
+			datapoints := make([]*datasetsv1.CopernicusDataspaceGranule, 0)
+			for datapoint, err := range data {
+				if err != nil {
+					b.Fatalf("failed to load datapoint: %v", err)
+				}
+				r := &datasetsv1.CopernicusDataspaceGranule{}
+
+				err = proto.Unmarshal(datapoint.Data, r)
+				if err != nil {
+					b.Fatalf("failed to unmarshal datapoint: %v", err)
+				}
+				datapoints = append(datapoints, r)
+			}
+		}
+	})
+
+	b.Run("No marshal and no reflection", func(b *testing.B) {
+		for range b.N {
+			data := client.Datapoints.Load(ctx, collectionID, loadInterval)
+			datapoints := make([]*RawDatapoint, 0)
+			for datapoint, err := range data {
+				if err != nil {
+					b.Fatalf("failed to load datapoint: %v", err)
+				}
+				datapoints = append(datapoints, datapoint)
+			}
+		}
+	})
 }
